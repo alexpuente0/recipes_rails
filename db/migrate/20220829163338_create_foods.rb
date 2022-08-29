c8e8b4ff5f66dc55bb23d6e03b5f0bd125168d0a
arturo <ahpareja@gmail.com>
2022-08-29 12:14:29 -0500

    Create database schema

--- db/migrate/20220829163338_create_foods.rb
+++ db/migrate/20220829163338_create_foods.rb
@@ -1,7 +1,12 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-      
+      t.belongs_to :user, foreign_key: true
+      t.string :name
+      t.numeric :measurement_unit
+      t.numeric :price
+      t.numeric :quantity
+
       t.timestamps
     end
   end
--- db/migrate/20220829163506_create_recipes.rb
+++ db/migrate/20220829163506_create_recipes.rb
@@ -1,6 +1,13 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
+      t.belongs_to :user, foreign_key: true
+
+      t.string :name
+      t.string :preparation_time
+      t.string :cooking_time
+      t.string :description
+      t.boolean :public, default: false
 
       t.timestamps
     end
