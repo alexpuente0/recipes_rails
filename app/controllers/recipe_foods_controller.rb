40c2d5443b94fc023c4b44d19e623dc6eea962a8
arturo <ahpareja@gmail.com>
2022-08-31 12:58:40 -0500

    bug fix add recipe_food form

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -4,8 +4,6 @@ class RecipeFoodsController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.find(params[:id])
-    @recipe_food.recipe_id = @recipe.id
     @recipe_food = RecipeFood.new(recipe_food_params)
     if @recipe_food.save
       redirect_to recipe_path(@recipe_food.recipe)
@@ -17,6 +15,6 @@ class RecipeFoodsController < ApplicationController
   private
 
   def recipe_food_params
-    params.require(:recipe_food).permit(:recipe_id, :food_id, :quantity)
+    params.require(:recipe_food).permit(:food_id, :quantity).merge(recipe_id: params[:recipe_id])
   end
 end
